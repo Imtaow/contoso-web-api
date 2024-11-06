ff72cbf80ef6f9fa064f99630f889bb803f8fe6f
Lathaphon taow <taow6504@gmail.com>
2024-11-06 08:49:37 +0000

    First commit

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = 8000;
 const HOST = '0.0.0.0'
 const app = express();
 app.get('/',(req,res)=> {
-    res.send('Hello World - 1.0.0');
+    //res.send('Hello World - 1.0.0');
+    es.sendFile(path.join(__dirname, 'test.html'));
 
 });
 app.listen(PORT,HOST,() => {
